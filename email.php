c6233ee43e265b23a584b5fbe88045c1f7d595ab
Kay Bi <144762035+Kalayar-Thin@users.noreply.github.com>
2025-11-03 15:08:22 +0630

    Update email.php

--- email.php
+++ email.php
@@ -2,12 +2,5 @@
 
 echo "sending email";
 echo "another email sent" ;
-echo "thid line from cli";
-echo "third line from onlne"
-echo "fourht line from cli";
-echo "third line from online";
-echo "fourth line form online";
-echo "fifth line from online";
-echo "fifth line from cli";
 
 ?>
